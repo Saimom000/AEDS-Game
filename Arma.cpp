4d4bd83286fc9bf75061b8bf1abfe92bd9d5c1de
Saimom000 <72466102+Saimom000@users.noreply.github.com>
2021-06-23 19:28:03 -0300

    Armas

--- Arma.cpp
+++ Arma.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
 #include <string>
-
+#include "Accessdata.cpp"
+#include <ctime>
 using namespace std;
 
 class Arma
 {
-private:
+protected:
     int id;
     string nome;
     int danomin;
     int danomax;
     int durabilidade;
-    int *tipoperso;
+    int *listaPerso;
 
 public:
-    Arma(/* args */);
+    Arma(string nomeArma);
+    virtual int CalcularDano();
     ~Arma();
 };
 
-Arma::Arma(/* args */)
+Arma::Arma(string nomeArma)
 {
+    srand(time(0));
+    this->nome = nomeArma;
 
+    int qtdAtributos = 10;
+    Accessdata *a = new Accessdata(qtdAtributos);
+    int *atributos = a->obtemAtributos(nomeArma);
+    this->danomax = atributos[0];
+    this->danomin = atributos[1];
+    this->durabilidade = 3;
+    this->listaPerso = new int[8];
+    int j = 0;
+    for (int i = 2; i < qtdAtributos; i++)
+    {
+        listaPerso[j++] = atributos[i];
+    }
+    
+    delete a;
+    delete atributos;
 }
-
+int Arma::CalcularDano(/* args */)
+{
+    this->durabilidade -=1;
+    return (this->danomin +(rand() % (this->danomax - this->danomin + 1)));
+    
+}
 Arma::~Arma()
 {
 }
--- Personagem.cpp
+++ Personagem.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <ctime>
 #include "Arma.cpp"
 #include "Magia.cpp"
 #include "Accessdata.cpp"
@@ -14,7 +15,9 @@ private:
     string classe;
     string nome;
     int vida;
+    int maxmana;
     int mana;
+    int maxvida;
     int forca;
     int magiah;
     int armadura;
@@ -25,54 +28,74 @@ private:
     string abreArquivo(string nomeClasse);
     int *obtemAtributos(string nomeClasse);
     int calcularDesvio(int agilidade);
-
+    int verificaVida();
 public:
     Personagem(string classePersonagem);
-    int atacar();
-    int receberDano(int dano, int agilidade);
-    ~Personagem();
+    int atacarArma();
+    int receberDano(int dano,int ataque);
+    // ~Personagem();
 };
 
 Personagem::Personagem(string classePersonagem)
 {
+    srand(time(0));
     int qtdAtributos = 8;
     Accessdata *a = new Accessdata(qtdAtributos);
     int *atributos = a->obtemAtributos(classePersonagem);
     this->id = atributos[0];
     this->vida = atributos[1];
+    this->maxvida = atributos[1];
     this->mana = atributos[2];
+    this->maxmana = atributos[2];
     this->forca = atributos[3];
     this->magiah = atributos[4];
     this->armadura = atributos[5];
     this->resistencia = atributos[6];
     this->agilidade = atributos[7];
 
-    // delete[] a;
+    // delete a;
+    // delete atributos;
 }
 
-int Personagem::calcularDesvio(int agilidade)
+int Personagem::calcularDesvio()
 {
     int a;
-    a = (rand() % 100);
-    if (a <= agilidade)
+    a = (rand() % 100)+1;
+    if (a <= this.agilidade)
     {
         return 1; // desvia
     }
 
     return 0; //nao desvia
 }
+int Personagem::atacarArma(){
+    int danoArma =0;
 
-int Personagem::receberDano(int dano, int agilidade)
+    int datoTolta = danoArma + (danoArma* (this->forca/100))
+}
+int Personagem::receberDano(int dano, int ataque)
 {
 
-    int esquiva = calcularDesvio(agilidade);
-    if(esquiva == 0){
-        int danoArmaRecebido = dano * 0.2;
-        if (dano > this->vida)
-            this->vida = 0;
-        else
-            this->vida -= dano;
+    int esquiva = calcularDesvio();
+    if(esquiva == 1){
+        return 0;
     }
 
-    return  0;
+    int reducaodano;
+    if(ataque == 0)
+        reducaodano = this->resistencia /100;
+    else
+        reducaodano = this->armadura/100;
+
+    int danoRecebido = dano - (dano * (reducaodano);
+
+        if (danoRecebido > this->vida)
+            this->vida = 0;
+        else
+            this->vida -= danoRecebido;
+
+    return  danoRecebido;
+}
+int Personagem::verificaVida(){
+    return this->vida;
 }
